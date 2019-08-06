e004db0b53eaaa546f432db3e48ce6fd6b670cb2
brandonbellanti <brandonbellanti@gmail.com>
2019-08-05 20:50:08 -0400

    Groupings.

--- scripts/analyzer.py
+++ scripts/analyzer.py
@@ -5,19 +5,23 @@ import matplotlib.pyplot as plt
 csv_file = 'the_office-all_episodes-database.csv'
 df = pandas.read_csv(csv_file)
 
+gb = df.groupby(['season','character'])
+print(gb['words'].sum())
 
 # # plot character
-# michael = df[df.character == 'Michael']
-# michael.plot.bar(x='episode', y='lines')
+# character = df[(df.character == 'Jim')]
+# seasons = character.groupby('season')
+# means = seasons.words.mean()
+# means.plot.bar(x='episode', y='lines')
 # plt.show()
 
 
-# plot entire season
-season_four = df[df.season == 4]
-characters = season_four.groupby(['character'])
-means = characters.words.mean()
-means.plot(kind='bar', x='character',y='words')
-plt.show()
+# # plot entire season
+# season_four = df[df.season == 4]
+# characters = season_four.groupby(['character'])
+# means = characters.words.mean()
+# means.plot(kind='pie', x='character',y='words')
+# plt.show()
 
 
 # # plot specific episode
